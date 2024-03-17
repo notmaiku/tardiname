3838160f10515f6c7ae335fbb94dde9411ec084d
notmaiku <mike1davis@rocketmail.com>
2024-03-17 03:04:59 -0400

    fetching localhost data from gleam server

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import styles from './App.module.css';
 import NameInput from './NameInput';
 import { nameShared } from './NameInput';
+import { createSignal, createEffect } from 'solid-js';
 
 function App() {
   const  [ nameRef ] = nameShared;
+  const [ data, setData] = createSignal('');
+  createEffect(async () => { try {
+      const response = await fetch(import.meta.env.VITE_BACKEND_URL);
+      const jsonData = await response.text()
+      setData(jsonData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  });
+
   return (
     <div class={styles.App}>
+      <h1>{data()}</h1>
       <NameInput />
       {/* Todo: implement a backend call to return a name from the server */}
       <button type="button" onClick={() => console.log(nameRef())}>Generate Name</button>
